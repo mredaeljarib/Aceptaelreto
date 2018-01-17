49d801cdde2ce5b913cb0e2cd713c52fa720ea06
mredaeljarib <35342659+mredaeljarib@users.noreply.github.com>
2018-01-17 20:03:55 +0100

    Add files via upload
    Añadido Hangares

---
+++ AceptaElReto/src/OrganizandoLosHangares/OrganizandoLosHangares.java
@@ -0,0 +1,54 @@
+package os;
+
+import java.util.Scanner;
+
+public class OrganizandoLosHangares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner tec= new Scanner(System.in);
+
+		int n=1;
+		
+		int v=0;
+		
+		int s=0;
+		int s1=0;
+		while(n!=0){
+			n=tec.nextInt();
+			if(n==0){
+				
+			}
+			else{
+				
+				for(int q=0;q<n;q++){
+					
+					v=tec.nextInt();
+					s=s+v;
+					
+					
+				}
+				
+				n=tec.nextInt();
+				
+				for(int q=0;q<n;q++){
+					
+					v=tec.nextInt();
+					s1=s1+v;
+					
+				}
+				
+				if(s>=s1){
+					System.out.println("SI");
+				}
+				else{
+					System.out.println("NO");
+				}				
+				s=s1=0;
+			}
+			
+		}
+		
+	}
+
+}
