06c89ab55cf504f108c562eca4aaa458e0dddb2a
mredaeljarib <35342659+mredaeljarib@users.noreply.github.com>
2018-01-17 18:03:58 +0100

    Update TendenciaAlLadoOscuro.java
    Programa final TendenciaAlLadoOscuro.

--- AceptaElReto/src/TendenciaAlLadoOscuro/TendenciaAlLadoOscuro.java
+++ AceptaElReto/src/TendenciaAlLadoOscuro/TendenciaAlLadoOscuro.java
@@ -4,26 +4,45 @@
  * and open the template in the editor.
  */
 package TendenciaAlLadoOscuro;
-
+import java.util.Scanner;
 /**
  *
  * @author mohbre
  */
 public class TendenciaAlLadoOscuro {
     public static void main(String[] args) {
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        Scanner tec= new Scanner(System.in);
+		int cont = 0;
+		int n=0;
+		int v=0;
+		int s=0;
+		String r="";
+		n=tec.nextInt();
+		for(int i=0;i<n;i++){//pasamos a base 5
+			v=tec.nextInt();
+			r="";
+			while(v>5){
+				
+				s=v%5;
+				v=v/5;
+				r=s+r;
+				
+			}
+			r=v+r;
+			//System.out.println(r);
+			
+			cont=0;//contamos los 4
+			for(int q=0;q<r.length();q++){
+				if('4'==r.charAt(q)){
+					cont++;
+				}
+			}
+			if(cont>1){
+				System.out.println("SI");
+			}
+			else{
+				System.out.println("NO");
+			}
+		}
     }
 }
