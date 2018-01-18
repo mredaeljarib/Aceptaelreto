5a626f4ea2bc51a671a597879a87eb959b6ec8ea
mredaeljarib <35342659+mredaeljarib@users.noreply.github.com>
2018-01-18 19:09:48 +0100

    Update OrganizandoLosHangares.java
    Corregido

--- AceptaElReto/src/OrganizandoLosHangares/OrganizandoLosHangares.java
+++ AceptaElReto/src/OrganizandoLosHangares/OrganizandoLosHangares.java
@@ -4,51 +4,60 @@ import java.util.Scanner;
 
 public class OrganizandoLosHangares {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner tec= new Scanner(System.in);
+    static boolean Caso(int[] v, int[] z) {
 
-		int n=1;
-		
-		int v=0;
-		
-		int s=0;
-		int s1=0;
-		while(n!=0){
-			n=tec.nextInt();
-			if(n==0){
-				
-			}
-			else{
-				
-				for(int q=0;q<n;q++){
-					
-					v=tec.nextInt();
-					s=s+v;
-					
-					
-				}
-				
-				n=tec.nextInt();
-				
-				for(int q=0;q<n;q++){
-					
-					v=tec.nextInt();
-					s1=s1+v;
-					
-				}
-				
-				if(s>=s1){
-					System.out.println("SI");
-				}
-				else{
-					System.out.println("NO");
-				}				
-				s=s1=0;
-			}
-			
-		}
-		
-	}
+        for (int i = 0; i < z.length; i++) {
+            if (! Entra(v, z[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean Entra(int[] v, int tamaño) {
+        int pos = MAX(v);
+        if (v[pos] < tamaño) {
+            return false;
+        } else {
+            v[pos] = v[pos] - tamaño;
+            return true;
+        }
+    }
+
+    static int MAX(int[] v) {
+        int max = Integer.MIN_VALUE;
+        int p = 0;
+        for (int i = 0; i < v.length; i++) {
+            if (max < v[i]) {
+                max = v[i];
+                p = i;
+            }
+        }
+        return p;
+    }
+
+   public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner tec = new Scanner(System.in);
+        int n;
+        while ( (n = tec.nextInt()) != 0) {
+
+            int[] v = new int[n];
+            for (int i = 0; i < n; i++) {
+                v[i] = tec.nextInt();
+            }
+            n = tec.nextInt();
+            int[] z = new int[n];
+            for (int i = 0; i < n; i++) {
+                z[i] = tec.nextInt();
+            }
+            if (Caso(v, z)) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
 
 }
+
